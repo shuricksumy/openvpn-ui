9e761dbc4589dd52b33303ee500eec0105238908
Your Name <you@example.com>
2023-08-10 03:16:38 +0300

    update build script
    Former-commit-id: 53fee2d03db9103f39127ee1077ed5632fac4732 [formerly dafcb648b1b16dffcff237a27d31c484f4c106cc] [formerly 73472f5d854d7f3e87930af47fd897cfbf542c7c [formerly 4d094768b0faae2af326cc601d4d6cadd295d6b7]]
    Former-commit-id: 0b2495dbdf4d023fedf9a09ffff0645c62314b50 [formerly 0c09d0f60989c5048b7ee0080e103fcd51264d80]
    Former-commit-id: 87deff9393ecd9270f569f0cc9bbd2fdeeca5fd9

--- build_local/build_part_2.sh
+++ build_local/build_part_2.sh
@@ -4,5 +4,5 @@ export GOPATH=`pwd`
 
 cd ./src/github.com/d3vilh/openvpn-ui
 
-$GOPATH/bin/bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'
+$GOPATH/bin/bee pack -exr='^vendor|^data.db|^build|^README.md|^docs|^README_ORIGINAL.md|^screenshots'
 
