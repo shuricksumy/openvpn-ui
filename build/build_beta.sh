c08eba20982100d4190f652c97bb13faafa06407
shuricksumy <suchkov.av@gmail.com>
2023-09-18 03:09:14 +0300

    Fix bug

--- build/build_beta.sh
+++ build/build_beta.sh
@@ -9,7 +9,7 @@ fi
 docker login
 # docker buildx build --platform linux/amd64,linux/arm64 -f Multi-arch.dockerfile -t shuricksumy/openvpn-ui:beta . --push --no-cache
 
-docker buildx build BRANCH=$1 --platform linux/amd64 -f Multi-arch-beta.dockerfile -t shuricksumy/openvpn-ui:beta . --push --no-cache
+docker buildx build --build-arg BRANCH=$1 --platform linux/amd64 -f Multi-arch-beta.dockerfile -t shuricksumy/openvpn-ui:beta . --push --no-cache
 
 
 # Single-arch (amd64) development build
