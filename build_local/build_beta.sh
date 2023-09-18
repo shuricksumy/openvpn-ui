50b91057096f483acaffa7242ed5953841bb70ec
shuricksumy <suchkov.av@gmail.com>
2023-09-18 03:03:55 +0300

    Add beta builder

--- build_local/build_beta.sh
+++ build_local/build_beta.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "ERROR: Need parametr - beta_branch_name !!!"
+fi 
+
+
+
 mkdir -p ./src/github.com/shuricksumy/openvpn-ui
 cd ./src/github.com/shuricksumy/openvpn-ui
 git clone https://github.com/shuricksumy/openvpn-ui.git ./
-git checkout routing_mgmt
+git checkout $1 
 
 
 cd ./build
 
 docker buildx create --use
 
-./build_beta.sh
+./build_beta.sh $1
