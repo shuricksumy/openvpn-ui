ad05d9e9d6fc08e6af4f31b4d8022d2376d62012
Oleksandr Suchkov <suchckov.av@gmail.com>
2023-12-03 02:38:03 +0000

    Update

--- build/assets/start.sh
+++ build/assets/start.sh
@@ -63,13 +63,13 @@ else
     fi
 fi
 
-if [ ! -f ${OVDIR}/clientDetails.json ]; then
-    touch ${OVDIR}/clientDetails.json
-fi
+# if [ ! -f ${OVDIR}/clientDetails.json ]; then
+#     touch ${OVDIR}/clientDetails.json
+# fi
 
-if [ ! -f ${OVDIR}/routesDetails.json ]; then
-    touch ${OVDIR}/routesDetails.json
-fi
+# if [ ! -f ${OVDIR}/routesDetails.json ]; then
+#     touch ${OVDIR}/routesDetails.json
+# fi
 
 # wait for openvpn server ready
 # until [ -f ${OVDIR}/server.conf ]
